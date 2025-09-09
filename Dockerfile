4368756f5e510507b3bb3a45a0eae8eeb494b4db
zeyad <zeyadashraf1115@gmail.com>
2025-09-09 22:41:11 +0300

    Initial commit for React DevOps project

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+from node:16-alpine AS build
+WORKDIR /app
+COPY package*.json ./
+RUN npm install
+COPY . .
+RUN npm run build
+FROM nginx:alpine
+COPY --from=build /app/build /usr/share/nginx/html
+COPY nginx.conf /etc/nginx/conf.d/default.conf
+EXPOSE 3000
+CMD ["nginx", "-g", "daemon off;"]
+
+
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,24 +1,9 @@
-import React, { useState, useEffect } from 'react';
-
-import Login from '../Login';
-import Dashboard from '../Dashboard';
+import React from "react";
+import Dashboard from "../Dashboard";
 
 const App = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(null);
-
-  useEffect(() => {
-    setIsAuthenticated(JSON.parse(localStorage.getItem('is_authenticated')));
-  }, []);
-
-  return (
-    <>
-      {isAuthenticated ? (
-        <Dashboard setIsAuthenticated={setIsAuthenticated} />
-      ) : (
-        <Login setIsAuthenticated={setIsAuthenticated} />
-      )}
-    </>
-  );
+  return <Dashboard />;
 };
 
 export default App;
+
